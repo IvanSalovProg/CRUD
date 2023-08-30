bbccd0eec674790ff15ae9b7e6fc44ea4f88ebc6
Ivan <IvanSalov94@gmail.com>
2023-08-30 12:12:21 +0500

    Initial commit

---
+++ CRUD/src/main/java/com/example/CRUD/CrudApplication.java
@@ -0,0 +1,18 @@
+package com.example.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
