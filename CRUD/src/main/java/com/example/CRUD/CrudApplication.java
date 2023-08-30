9a56ef1f041dc4d402e89795a90cf4f50ffd8122
Ivan <IvanSalov94@gmail.com>
2023-08-30 12:16:21 +0500

    remove 2 import

--- CRUD/src/main/java/com/example/CRUD/CrudApplication.java
+++ CRUD/src/main/java/com/example/CRUD/CrudApplication.java
@@ -2,8 +2,7 @@ package com.example.CRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 
 
 
